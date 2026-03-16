1195be862e1269225596558a3c922879931dd380
OnO-International <hibiki20240414@gmail.com>
2026-03-16 09:19:16 +0900

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ono-international.com',
   base: '/',
   output: 'static',
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [sitemap()],
 });
